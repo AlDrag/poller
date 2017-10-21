79fbeb99cacdfc22387d2935e50a485bac364e39
Alan Dragicevich <aldragicevich@gmail.com>
2017-10-22 07:59:39 +1300

    Fixed PollServiceSpec

--- src/app/polls/poll.service.spec.ts
+++ src/app/polls/poll.service.spec.ts
@@ -1,15 +1,17 @@
-// import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-// import { PollService } from './poll.service';
+import { PollService } from './poll.service';
 
-// describe('PollService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [PollService]
-//     });
-//   });
+describe('PollService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PollService]
+    });
+  });
 
-//   it('should be created', inject([PollService], (service: PollService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
+  it('should be created', inject([PollService], (service: PollService) => {
+    expect(service).toBeTruthy();
+  }));
+});
